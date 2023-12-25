071df2135bd2aaa8f0e8049987c403dc0b0557ce
arctic-aurora123 <110533379+arctic-aurora123@users.noreply.github.com>
2023-12-25 12:36:25 +0800

    Add files via upload
    updated the sdf 
    add the road model and the contact sensor

---
+++ Gazebo_Race/CMakeFiles/race_node.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arctic/Projects/Gazebo_Race/race_node.cc" "CMakeFiles/race_node.dir/race_node.cc.o" "gcc" "CMakeFiles/race_node.dir/race_node.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Gazebo_Race/CMakeFiles/race_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/race_node.dir/race_node.cc.o"
+  "CMakeFiles/race_node.dir/race_node.cc.o.d"
+  "race_node"
+  "race_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/race_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
